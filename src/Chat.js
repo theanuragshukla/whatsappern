bd88d33072cb94ee2230a2af483dda2ab85018d6
Anurag Shukla <tachyonanurag@gmail.com>
2022-07-20 17:22:28 +0530

    /chat css almost complete

--- src/Chat.js
+++ src/Chat.js
@@ -1,16 +1,18 @@
 import {NavMain ,DpDiv,Brand , Icons, Icon} from './navComponents'
-import {VoiceCall,VideoCall,OptionsIcon,BackIcon,SendIcon} from './icons/AllIcons'
-import {ChatMain,Chats,InputMain,InputDiv,Send} from './ChatComponents'
-const Chat = () => {
+import { Link} from 'react-router-dom'
+import {VoiceCall,VideoCall,OptionsIcon,BackIcon,SendIcon,EmojiIcon} from './icons/AllIcons'
+import {ChatMain,Chats,InputMain,InputDiv,Send,Input, MsgDiv,Msg,ChatBody} from './ChatComponents'
+const Chat = () => { 
 	return (
 		<ChatMain>
 		<NavMain>
 		<Brand>
-		<Icons className="backIcon">
+		<Link to='/'>
+<Icons className="backIcon">
 		<Icon className="backIcon">
 		<BackIcon className="backIcon"></BackIcon>
 		</Icon>
-		</Icons>
+		</Icons></Link>
 		<DpDiv>
 		<div></div>
 		</DpDiv>
@@ -22,19 +24,35 @@ const Chat = () => {
 		<Icon><OptionsIcon/></Icon>
 		</Icons>
 		</NavMain>
-		<Chats></Chats>
+		<ChatBody>
+<Chats>
+
+		<MsgDiv>
+		<Msg>
+Elit voluptatum ex fuga autem distinctio blanditiis totam. Harum delectus excepturi maiores itaque error Reprehenderit illum debitis ducimus fuga iure Minus quo pariatur elit rem labore Magni vero error repellendus
+		</Msg>
+		<div className="time">05:09pm</div>
+		</MsgDiv>
+
+		<MsgDiv right>
+		<Msg>
+Elit voluptatum ex fuga autem distinctio blanditiis totam. Harum delectus excepturi maiores itaque error Reprehenderit illum debitis ducimus fuga iure Minus quo pariatur elit rem labore Magni vero error repellendus
+		</Msg>
+		<div className="time">05:09pm</div>
+		</MsgDiv>
+		</Chats></ChatBody>
 		<InputMain>
 		<InputDiv>
 		<Icons>
 		<Icon>
-		<OptionsIcon/>
+		<EmojiIcon />
 		</Icon>
 		</Icons>
+		<input type='text' placeholder='Message'/>
 		</InputDiv>
 		<Send>
-
 		<Icons><Icon>
-		<SendIcon></SendIcon>
+		<SendIcon/>
 		</Icon>
 		</Icons>
 		</Send>
--- src/ChatComponents.js
+++ src/ChatComponents.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled,{css} from 'styled-components'
 
 const ChatMain = styled.div`
 width:100%;
@@ -6,11 +6,23 @@ height:100vh;
 overflow:hidden;
 `
 
-const Chats = styled.div`
+const ChatBody = styled.div`
 background-color:var(--gray);
 height:calc(100vh - 125px);
+display: flex;
+	width: 100%;
+	flex-direction:column;
+	overflow:scroll ;
+	justify-content:flex-start;
 `
 
+const Chats = styled.div`
+background-color:var(--gray);
+display: flex;
+	width: 100%;
+	flex-direction:column;
+	justify-content:flex-start;
+`
 const InputMain = styled.div`
 	width: 100%;
 	height: 65px;
@@ -19,16 +31,49 @@ const InputMain = styled.div`
 	position:absolute;
 	background-color:var(--gray);
 	display: flex;
-	
+
 `
 const InputDiv = styled.div`
 width: calc(100% - 55px);
 	height: 55px;
+	display:flex;
 	border-radius:200vmax ;
 	flex-direction: row;
 	background-color: #fff;
 	overflow: hidden;
+	justify-content:flex-start;
+
+&>div{
+width:55px;
+}
+	& svg{
+	fill:#666;
+	}
+	& > input{
+	width:calc(100% - 60px);
+	font-size: 1.05rem;
+	outline: none;
+	border: none;
+	padding-right: 20px;
+	color:#000;
+	}
+	&>input::placeholder{
+	color:#666;
+	}
 `
+const Input = styled.input`
+    ${props =>   props.type === 'text' && `
+        width: 100%;
+        height: 55px;
+        background-color: rgba(104, 105, 102, 0.1);
+        border: 1px solid #354545;
+        margin-top: 20px;
+        outline: none;
+        padding-left: 40px;
+        color: white;
+        font-size: 22px;
+    `}
+`;
 const Send = styled.div`
 height:55px;
 width:55px;
@@ -53,10 +98,46 @@ height:100%;
 
 }
 `
+const MsgDiv = styled.div`
+	display: inline-block;
+	min-width: 100px;
+	max-width: 70%;
+	padding: 5px 10px;
+	background-color:var(--light-lime);
+	height: auto;
+	margin-top: 10px;
+	border-radius:6px;
+	margin:auto;
+	margin:${props=>props.right ? `4px 5px auto auto` :`4px auto auto 5px`};
+
+	&>.time {
+	height: 16px;
+	font-size:0.7rem;
+	width: 100%;
+	text-align: right;
+	margin-top: 4px;
+	color:#666;
+	font-weight:500;
+}
+
+
+`
+const Msg = styled.div`
+	font-size: 1rem;
+	color: #000;
+	word-wrap: break-word;
+	overflow: hidden;
+
+
+`
 export {
 	InputMain,
+	ChatBody,
 	Chats,
 	ChatMain,
 	InputDiv,
+	Input,
+	MsgDiv,
+	Msg,
 	Send
 }
